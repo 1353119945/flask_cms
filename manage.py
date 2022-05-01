93d2500423f99ccca30c1ae408fe93b80c9662a9
Pancake_14 <1353119945@qq.com>
2022-05-01 15:21:32 +0800

    启动文件

--- manage.py
+++ manage.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask_script import Manager # 导入扩展，用于管理器启动项目
+
 app = Flask(__name__)
+manage = Manager(app) # 实例化管理器
 
 @app.route('/')
 def index():
     return 'hello HLB!'
 
 if __name__ == '__main__':
-    app.run()
+    manage.run()    #对应实例manage（没有实例化就是app.run() ）
