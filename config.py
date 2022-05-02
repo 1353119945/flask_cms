f15867a14ab6eec6e49a1a44b4ebf663c7d03004
Pancake_14 <84325537+1353119945@users.noreply.github.com>
2022-05-02 14:11:58 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 class Config:
     DEBUG = None
 
+    #配置数据库
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:abc123456@127.0.0.1:3306/cms' # 连接数据库
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
 # 定义开发模式的配置
 class developmentConfig(Config):
-    DEBUG = True
+    DEBUG = True    #由于上面入口文件读取的是开发环境的 DEBUG，我们设置的是 true，所以Debugger显示的是 active。如果改成 production，你会看到 DEBUG 为 off的状态
 
 # 定义生产模式的配置
 class productionConfig(Config):
--- manage.py
+++ manage.py
@@ -1,8 +1,13 @@
-from flask import Flask
 from flask_script import Manager # 导入扩展，用于管理器启动项目
+from flask_migrate import Migrate,MigrateCommand # 导入迁移包
 
-app = Flask(__name__)
+from app import create_app,db # 导入工厂函数create_app和db函数
+
+app = create_app("development") # 调用函数，获取实例，这里使用的是开发模式的app
+
 manage = Manager(app) # 实例化管理器
+Migrate(app,db) # 注入框架实例和数据实例
+manage.add_command('db',MigrateCommand) # 添加迁移命令
 
 @app.route('/')
 def index():
